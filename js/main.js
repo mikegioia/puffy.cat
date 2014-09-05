1d0b181d249dd5563bb12af2788b195d9f096fbf
Mike Gioia <mikegioia@gmail.com>
2014-09-05 17:31:30 -0400

    reformatted JS, main JS will need to be updated to look for random objects in the photos array and also break out captions.

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@ var fade = 1000,
     show = 3000;
 
 function randomImage() {
-  return "images/" + photos[Math.floor(Math.random() * photos.length)];
+    return "images/" + photos[ Math.floor( Math.random() * photos.length ) ];
 }
 
 function main() {
-  $("#photo").fadeOut(fade, function() {
-    $("#photo").css("background-image", "url('" + randomImage() +"')");
+    $( "#photo" ).fadeOut( fade, function () {
+        $( "#photo" ).css( "background-image", "url('" + randomImage() + "')" );
 
-    $("#photo").fadeIn(fade, function() {
-      setTimeout(main, show);
+        $( "#photo" ).fadeIn( fade, function () {
+            setTimeout( main, show );
+        });
     });
-  });
 }
 
 main();
