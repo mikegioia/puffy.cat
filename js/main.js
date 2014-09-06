91970de06660e588e61085a69cec92fb04d1d29f
Mike Gioia <mikegioia@gmail.com>
2014-09-06 14:45:05 -0400

    updated randomizer

--- js/main.js
+++ js/main.js
@@ -1,18 +1,38 @@
 var fade = 1000
-  , show = 3000;
+  , show = 3000
+  , photoCount = photos.length
+  , index = 0
+  , $photo = $( '#photo' )
+  , shuffled = [];
 
-function randomImage() {
-    return "images/" + photos[ Math.floor( Math.random() * photos.length ) ].file;
+function shuffle ( array ) {
+    for (
+        var j, x, i = array.length;
+        i;
+        j = Math.floor( Math.random() * i ),
+        x = array[ --i ],
+        array[ i ] = array[ j ],
+        array[ j ] = x );
+    return array;
+};
+
+function nextImage() {
+    var path = "images/" + shuffled[ index ].file;
+    index++;
+    if ( index >= photoCount ) {
+        index = 0;
+    }
+    return path;
 }
 
 function main() {
-    var $photo = $( "#photo" );
     $photo.fadeOut( fade, function () {
-        $photo.css( "background-image", "url('" + randomImage() + "')" );
+        $photo.css( "background-image", "url('" + nextImage() + "')" );
         $photo.fadeIn( fade, function () {
             setTimeout( main, show );
         });
     });
 }
 
+shuffled = shuffle( photos );
 main();
