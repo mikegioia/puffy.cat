8fd97b222f61fe3e53b1a9d4103a4fce7131b5f9
Mike Gioia <mikegioia@gmail.com>
2014-09-06 09:37:11 -0400

    adding photos, updated photos array

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-var fade = 1000,
-    show = 3000;
+var fade = 1000
+  , show = 3000;
 
 function randomImage() {
-    return "images/" + photos[ Math.floor( Math.random() * photos.length ) ];
+    return "images/" + photos[ Math.floor( Math.random() * photos.length ) ].file;
 }
 
 function main() {
-    $( "#photo" ).fadeOut( fade, function () {
-        $( "#photo" ).css( "background-image", "url('" + randomImage() + "')" );
-
-        $( "#photo" ).fadeIn( fade, function () {
+    var $photo = $( "#photo" );
+    $photo.fadeOut( fade, function () {
+        $photo.css( "background-image", "url('" + randomImage() + "')" );
+        $photo.fadeIn( fade, function () {
             setTimeout( main, show );
         });
     });
