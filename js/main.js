891011cbbd5c771fb07d41b79d79e30d38eef50c
Mike Gioia <mikegioia@gmail.com>
2019-07-18 19:35:32 -0700

    Rewrote entire app, this should be a lot more better

--- js/main.js
+++ js/main.js
@@ -1,38 +1,44 @@
-var fade = 1000
-  , show = 3000
-  , photoCount = photos.length
-  , index = 0
-  , $photo = $( '#photo' )
-  , shuffled = [];
+/**
+ * Puffy, I love you
+ */
 
-function shuffle ( array ) {
-    for (
-        var j, x, i = array.length;
-        i;
-        j = Math.floor( Math.random() * i ),
-        x = array[ --i ],
-        array[ i ] = array[ j ],
-        array[ j ] = x );
-    return array;
-};
+(function () {
+  var index = 0;
+  var fade = 1000;
+  var show = 3000;
+  var shuffled = [];
+  var photoCount = 45;
+  var $photo = document.getElementById('photo');
 
-function nextImage() {
-    var path = "images/" + shuffled[ index ].file;
-    index++;
-    if ( index >= photoCount ) {
-        index = 0;
-    }
-    return path;
-}
+  function shuffle ( array ) {
+      for (
+          var j, x, i = array.length;
+          i;
+          j = Math.floor( Math.random() * i ),
+          x = array[ --i ],
+          array[ i ] = array[ j ],
+          array[ j ] = x );
+      return array;
+  };
 
-function main() {
-    $photo.fadeOut( fade, function () {
-        $photo.css( "background-image", "url('" + nextImage() + "')" );
-        $photo.fadeIn( fade, function () {
-            setTimeout( main, show );
-        });
-    });
-}
+  function nextImage() {
+      var path = "images/" + shuffled[ index ].file;
+      index++;
+      if ( index >= photoCount ) {
+          index = 0;
+      }
+      return path;
+  }
 
-shuffled = shuffle( photos );
-main();
+  function main() {
+      $photo.fadeOut( fade, function () {
+          $photo.css( "background-image", "url('" + nextImage() + "')" );
+          $photo.fadeIn( fade, function () {
+              setTimeout( main, show );
+          });
+      });
+  }
+
+  shuffled = shuffle( photos );
+  main();
+}());
--- js/photos.js
+++ js/photos.js
@@ -1,271 +1,271 @@
 var photos = [{
-    file: "001.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "001.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "002.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "002.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "003.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "003.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "004.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "004.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "005.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "005.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "006.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "006.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "007.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "007.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "008.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "008.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "009.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "009.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "010.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "010.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "011.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "011.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "012.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "012.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "013.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "013.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "014.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "014.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "015.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "015.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "016.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "016.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "017.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "017.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "018.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "018.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "019.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "019.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "020.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "020.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "021.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "021.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "022.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "022.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "023.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "023.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "024.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "024.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "025.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "025.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "026.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "026.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "027.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "027.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "028.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "028.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "029.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "029.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "030.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "030.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "031.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "031.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "032.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "032.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "033.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "033.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "034.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "034.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "035.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "035.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "036.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "036.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "037.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "037.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "038.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "038.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "039.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "039.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "040.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "040.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "041.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "041.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "042.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "042.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "043.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    },
+  file: "043.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  },
 }, {
-    file: "044.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    }
+  file: "044.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  }
 }, {
-    file: "045.jpg",
-    desc: {
-        en: "",
-        ca: ""
-    }
+  file: "045.jpg",
+  desc: {
+    en: "",
+    ca: ""
+  }
 }];
